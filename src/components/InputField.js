cec892c8a8ed4da8ab437fd24a12bb5f475e795e
ndzib <serkwi.bruno@go-groups.net>
2023-12-06 12:04:05 +0100

    added react hook form

---
+++ src/components/InputField.js
@@ -0,0 +1,14 @@
+export const InputField = ({formProps , label, children, name='', placeholder = '', type = 'text' }) => {
+
+    return (
+        <div>
+            <label htmlFor="email-address-icon" className="block mb-2 font-medium text-gray-900">{label}</label>
+            <div className="relative">
+                <input {...formProps} name={name} type={type} id="email-address-icon" className="border border-gray-300 text-gray-900 text-sm rounded-lg outline-accent focus:ring-accent focus:border-accent block w-full pe-10 p-2.5" placeholder={placeholder}/>
+                <div className="absolute inset-y-0 end-0 flex items-center pe-3.5">
+                   {children}
+                </div>
+            </div>
+        </div>
+    )
+}
