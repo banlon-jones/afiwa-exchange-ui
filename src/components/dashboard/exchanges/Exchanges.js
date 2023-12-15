fab5ad2ea4ebebac5ef5e8af1cc3473d0ee93f0d
ndzib <serkwi.bruno@go-groups.net>
2023-12-15 17:35:46 +0100

    added support for managing rates

--- src/components/dashboard/exchanges/Exchanges.js
+++ src/components/dashboard/exchanges/Exchanges.js
@@ -1,5 +1,6 @@
 import { useQuery } from "react-query";
 import { useAxios } from "../../../data/api";
+import { Spinner } from "../../spinner/Spinner";
 import { FilterExchanges } from "./FilterExchanges";
 
 export const Exchanges = () => {
@@ -7,6 +8,7 @@ export const Exchanges = () => {
     const { data, isLoading } = useQuery('currentUser', getCurrentUser, { retry: 0 })
     return (
         <>
+            {isLoading && <div className="flex w-full min-h-[100%] items-center justify-center"> <Spinner /></div> }
             {data && <FilterExchanges exchanges={data?.data[0]?.transactions} />}
         </>
     )
--- src/components/dashboard/settings/rates/EditRate.js
+++ src/components/dashboard/settings/rates/EditRate.js
@@ -2,7 +2,7 @@ import { Icon } from "@iconify/react"
 import { useState } from "react"
 import { useForm } from "react-hook-form"
 
-export const EditRate = () => {
+export const EditRate = ({rate}) => {
     const [editing, setEditing] = useState(false)
     const {register, handleSubmit} = useForm()
 
@@ -12,11 +12,10 @@ export const EditRate = () => {
 
     return (
         <form onSubmit={handleSubmit(saveRate)} className="flex gap-4 items-center">
-            <input type="number" {...register('rate', {required: true, value: 50})} disabled={!editing} className="border border-3 rounded-xl px-4 py-2 text-right w-[150px] outline-accent" />
+            <input type="number" {...register('rate', {required: true, value: rate})} disabled={!editing} className="border border-3 rounded-xl px-4 py-2 text-right w-[150px] outline-accent" />
 
             {!editing &&
                 <button onClick={() => setEditing(true)} type="button" className="bg-secondary-1 py-2 px-4 rounded-xl"><Icon height={24} icon={'mdi:edit-outline'} /></button>
-
             }
 
             {
