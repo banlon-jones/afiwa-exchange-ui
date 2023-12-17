6e36d629d7982e1735554a7cd6c07665a2da538e
banlonjones <banlonjones@go-groups.net>
2023-12-17 01:52:24 +0100

    some ui touches

--- src/data/api.js
+++ src/data/api.js
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 import { SelectAuth } from "../store/slices/AuthSlice";
 import { useEffect } from "react";
 
-const apiUrl = 'http://localhost:3500/api'
+const apiUrl = 'http://ec2-3-92-51-85.compute-1.amazonaws.com:8080/api'
 
 export const useAxios = () => {
     const auth = useSelector(SelectAuth);
